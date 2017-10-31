fec52bac4e183b795eb7a11db61b11b848099294
Alferguson <alferguson@berkeley.edu>
2017-10-30 21:11:48 -0700

    with firebase google login

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,6 @@
+
+
+
 // Initialize Firebase
 var config = {
 	apiKey: "AIzaSyDEnV-n5umJb0UsHTXkfWpzeXeozNhi50Y",
@@ -8,6 +11,91 @@ var config = {
 	messagingSenderId: "942613125163"
 };
 firebase.initializeApp(config);
+
+
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithRedirect(provider);
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+// function login() {
+// 	function newLoginHappened(user) {
+// 		if (user) {
+// 			app(user);
+// 		} else {
+// 			var provider = new firebase.auth.GoogleAuthProvider();
+// 			firebase.auth().signInWithRedirect(provider);
+// 		}	
+// 	}
+
+// 	firebase.auth().onAuthStateChanged(newLoginHappened);
+// }
+
+
+// function app(user) {
+// 	document.getElementById("clientName").innerHTML = user.displayName;
+// }
+
+// window.onload = login;
+// google auth
+// function callGoogleSignIn() {
+// $("callGoogleSignIn").on("click", function(event){
+//     event.preventDefault();
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//       firebase.auth().signInWithRedirect(provider).then(function (result) {
+//             var token = result.credential.accessToken;
+//             // The signed-in user info.
+//             var user = result.user;
+           
+//         });
+//         firebase.auth().getRedirectResult().then(function (result) {
+//             if (result.credential) {
+//                 var token = result.credential.accessToken;
+//             }
+//             // The signed-in user info.
+//             var user = result.user;
+//             var provider = new firebase.auth.GoogleAuthProvider();
+//             provider.addScope('profile');
+//             provider.addScope('email');
+//             firebase.auth().signInWithRedirect(provider);
+//         }).catch(function (error) {
+//         });
+// });
+// }
+// firebase.auth().signOut().then(function() {
+//   // Sign-out successful.
+// }).catch(function(error) {
+//   // An error happened.
+// });
+// var user = firebase.auth().currentUser;
+// if (user != null) {
+//   user.providerData.forEach(function (profile) {
+//     console.log("Sign-in provider: "+profile.providerId);
+//     console.log("  Provider-specific UID: "+profile.uid);
+//     console.log("  Name: "+profile.displayName);
+//     console.log("  Email: "+profile.email);
+//     console.log("  Photo URL: "+profile.photoURL);
+//   });
+// }
 
 //keeps track of step of the process. We start on step "directions"
 //steps: directions, place-marker, select-station
@@ -97,7 +185,7 @@ function initMap() {
 		    location: newLocation,
 		    // location: {lat: 38.582, lng: -121.482},
 		    // location: latlng,
-		    radius: 5000,
+		    radius: 1000,
 		    type: ['gas_station']
 		}, callback);
 		// restaurant search
@@ -106,7 +194,7 @@ function initMap() {
 		    location: newLocation,
 		    // location: {lat: 38.582, lng: -121.482},
 		    // location: latlng,
-		    radius: 1000,
+		    radius: 5000,
 		    type: ['restaurant']
 		}, callback2);
 	}
